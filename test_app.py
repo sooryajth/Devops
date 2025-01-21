52a7ef93daff69d93f2938aad1f9446c786fb730
Sooryajith <sooryajithpblm@gmail.com>
2025-01-21 15:05:02 +0530

    updated

--- test_app.py
+++ test_app.py
@@ -1,10 +1,11 @@
 import unittest
 #HELLO KUNJUMANII
-#Hey its my first devops handson project
+#Hey its my first devops hands on project
 class TestApp(unittest.TestCase):
     def test_addition(self):
-    self.assertEqual(2 + 2, 4)
-
+        self.assertEqual(2 + 2, 4)
+    def test_subtraction(self):
+        self.assertEqual(3-2,1)
 if __name__ == "__main__":
     unittest.main()
 "# Test Change" 
