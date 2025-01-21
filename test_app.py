5729e62576274020a2c1519dbfcb2dff0c65919e
Sooryajith <sooryajithpblm@gmail.com>
2025-01-21 13:01:04 +0530

    updated

--- test_app.py
+++ test_app.py
@@ -3,7 +3,7 @@ import unittest
 #Hey its my first devops handson project
 class TestApp(unittest.TestCase):
     def test_addition(self):
-        self.assertEqual(2 + 2, 4)
+    self.assertEqual(2 + 2, 4)
 
 if __name__ == "__main__":
     unittest.main()
